fa390a697fe57e5977fd54afafce9ab93b78e2ea
jiangzhi <zhi.jiang@zkang-e.com>
2023-02-23 17:21:54 +0800

    调节xy

--- hlhk_sikulix/src/test/java/com/dz/DaoZhan.java
+++ hlhk_sikulix/src/test/java/com/dz/DaoZhan.java
@@ -1,26 +1,21 @@
 package com.dz;
-
+import org.sikuli.basics.HotkeyEvent;
+import org.sikuli.basics.HotkeyListener;
+import org.sikuli.script.Env;
 import org.sikuli.script.Key;
-import org.sikuli.script.Match;
 import org.sikuli.script.Region;
 
 //道战
 public class DaoZhan {
     public static void main(String[] args)throws Exception{
-        Region region = new Region(139, 609,728,264);
-        region.setThrowException(false);
-        while (true){
-            Thread.sleep(8000);
-            //寻找锁定
-            Match match = region.wait("D:/software/sikulix/image/suoding.PNG",5);
-            if(null != match){
-                //无极真气
-                region.type(Key.F5);
-                Thread.sleep(300);
-                //合击
-                region.type(Key.F4);
-            }
-        }
+        Env.addHotkey(Key.F4,  0, new ListenerTest());
+    }
+}
 
+class ListenerTest extends HotkeyListener {
+    public  Region region = new Region(0,0 ,1920 ,1080 );
+    @Override
+    public void hotkeyPressed(HotkeyEvent hotkeyEvent) {
+        region.type(Key.F5 );
     }
 }
