e31352c56c9393deb07e078c5af3bf0bf5f63cf6
466972777@qq.com <466972777@qq.com>
2023-02-25 09:42:27 +0800

    asdf

--- hlhk_sikulix/src/test/java/cesi/GlobleHotKeyDemo.java
+++ hlhk_sikulix/src/test/java/cesi/GlobleHotKeyDemo.java
@@ -1,6 +1,9 @@
 package cesi;
 import com.melloware.jintellitype.HotkeyListener;
 import com.melloware.jintellitype.JIntellitype;
+import org.sikuli.script.Key;
+import org.sikuli.script.Region;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -18,7 +21,7 @@ public class GlobleHotKeyDemo extends JFrame {
     public GlobleHotKeyDemo() {
 
         //第一步：注册热键，第一个参数表示该热键的标识，第二个参数表示组合键，如果没有则为0，第三个参数为定义的主要热键
-        JIntellitype.getInstance().registerHotKey(FUNC_KEY_MARK, 0, 65);
+        JIntellitype.getInstance().registerHotKey(FUNC_KEY_MARK, 0, 112);
 
         //第二步：添加热键监听器
         JIntellitype.getInstance().addHotKeyListener(new HotkeyListener() {
@@ -28,17 +31,24 @@ public class GlobleHotKeyDemo extends JFrame {
                     Robot robot = new Robot();
                     switch (markCode) {
                         case FUNC_KEY_MARK:
-                            robot.keyPress(KeyEvent.VK_Q);
+                            System.out.println("执行~~~~~");
                             robot.keyPress(KeyEvent.VK_W);
-                            robot.keyPress(KeyEvent.VK_E);
+                            Thread.sleep(500);
+                            System.out.println("111111111~~~~~");
+                            robot.keyPress(KeyEvent.VK_D);
+                            Thread.sleep(3000);
+                            System.out.println("222222~~~~~");
                             robot.keyPress(KeyEvent.VK_R);
                             break;
                     }
-                } catch (AWTException e) {
+
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
+
             }
         });
+
     }
     public static void main(String[] args) {
         new GlobleHotKeyDemo();
