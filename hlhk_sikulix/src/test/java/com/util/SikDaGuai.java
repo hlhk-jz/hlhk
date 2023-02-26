b723d0d993b314fc0d3da1b0ee84913577d875ee
466972777@qq.com <466972777@qq.com>
2023-02-26 08:33:38 +0800

    asdf

--- hlhk_sikulix/src/test/java/com/util/SikDaGuai.java
+++ hlhk_sikulix/src/test/java/com/util/SikDaGuai.java
@@ -53,9 +53,9 @@ public class SikDaGuai {
                     }
                 }
                 //嗜血术
-                Thread.sleep(500);
+                /*Thread.sleep(500);
                 region.type(Key.F7);
-                Thread.sleep(800);
+                Thread.sleep(800);*/
                 //合击
                 region.type(Key.F3);
             }
@@ -67,6 +67,7 @@ public class SikDaGuai {
                 matchyd.setY(matchyd.getY()-377);
                 matchyd.setX(matchyd.getX()+400);
                 matchyd.rightClick();
+                region.type(Key.F4);
                 match = region.wait(CurrencyData.tldJZ,2);
                 if(null == match){
                     //跟随寻找教主
@@ -75,12 +76,15 @@ public class SikDaGuai {
                     matchyd.setY(matchyd.getY()-377);
                     matchyd.rightClick();
                     match = region.wait(CurrencyData.tldJZ,3);
+                    region.type(Key.F4);
                     if(null == match){
                         matchyd = region.wait("D:/software/sikulix/tulongdian/tldyd.PNG",1);
                         matchyd.setY(matchyd.getY()-150);
                         matchyd.setX(matchyd.getX()+180);
                         matchyd.rightClick();
+                        Thread.sleep(300);
                         matchyd.rightClick();
+                        region.type(Key.F4);
                     }
                 }
             }
