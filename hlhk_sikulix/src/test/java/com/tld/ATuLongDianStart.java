4848de9025f08521facf21a4edcfcedf7622c13c
466972777@qq.com <466972777@qq.com>
2023-03-02 20:31:26 +0800

    asdf

--- hlhk_sikulix/src/test/java/com/tld/ATuLongDianStart.java
+++ hlhk_sikulix/src/test/java/com/tld/ATuLongDianStart.java
@@ -15,6 +15,8 @@ public class ATuLongDianStart {
 
     public static void tld(Region region){
         try {
+            //检查宝宝是否在线
+            BaoBao.baobao(region);
             //将F12移动到聊天栏
             ChunShi.yd(region);
             //检查药品
