65f550c4c3ed52f69cd0d7ef214b9615612a133d
466972777@qq.com <466972777@qq.com>
2023-03-04 10:14:01 +0800

    asdf

--- hlhk_sikulix/src/test/java/com/util/CurrencyData.java
+++ hlhk_sikulix/src/test/java/com/util/CurrencyData.java
@@ -1,8 +1,6 @@
 package com.util;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class CurrencyData {
     //屠龙殿教主标识
     public static String tldJZ = "D:/software/sikulix/image/jiaozhu2.PNG";
@@ -16,8 +14,6 @@ public class CurrencyData {
     public static String tldNpc = "D:/software/sikulix/tulongdian/tld.PNG";
     //新手福利
     public static String xsNPC = "D:/software/sikulix/image/fuli.PNG";
-    //土城NPC
-    public static String tcNpc = "D:/software/sikulix/image/tc.PNG";
     //装备栏随机石
     public static String zblSjs = "D:/software/sikulix/image/shitou.PNG";
 
@@ -38,5 +34,86 @@ public class CurrencyData {
         initList.add("D:/software/sikulix/image/gb9.PNG");
         initList.add("D:/software/sikulix/image/gb11.PNG");
         return initList;
+    }
+
+    //矿泉水属于饮料不
+    public static String KQS = "D:/software/sikulix/huishou/kqs.PNG";
+    public static String KQS1 = "D:/software/sikulix/huishou/kqs.PNG";
+    //一天多少小时
+    public static String XS = "D:/software/sikulix/huishou/xs.PNG";
+    public static String XS1 = "D:/software/sikulix/huishou/xs1.PNG";
+    //赵本山哪国人
+    public static String NGR = "D:/software/sikulix/huishou/ngr.PNG";
+    public static String NGR1 = "D:/software/sikulix/huishou/ngr1.PNG";
+    //爸爸的爸爸叫什么
+    public static String JSM = "D:/software/sikulix/huishou/jsm.PNG";
+    public static String JSM1 = "D:/software/sikulix/huishou/jsm1.PNG";
+    public static List<Object> san1Str(){
+        List<Object> list = new ArrayList<>();
+        list.add(KQS);
+        list.add(XS);
+        list.add(NGR);
+        list.add(JSM);
+        return list;
+    }
+    //被蚊子叮会不会痒
+    public static String WZ = "D:/software/sikulix/huishou/wz.PNG";
+    public static String WZ1 = "D:/software/sikulix/huishou/wz1.PNG";
+    //除夕夜吃饺子
+    public static String CJZ = "D:/software/sikulix/huishou/cjz.PNG";
+    public static String CJZ1 = "D:/software/sikulix/huishou/cjz1.PNG";
+    //多少岁内是未成年
+    public static String WCN = "D:/software/sikulix/huishou/wcn.PNG";
+    public static String WCN1 = "D:/software/sikulix/huishou/wcn1.PNG";
+    //企鹅会飞不
+    public static String QE = "D:/software/sikulix/huishou/qe.PNG";
+    public static String QE1 = "D:/software/sikulix/huishou/qe1.PNG";
+    public static List<Object> san4Str(){
+        List<Object> list = new ArrayList<>();
+        list.add(WZ);
+        list.add(CJZ);
+        list.add(WCN);
+        list.add(QE);
+        return list;
+    }
+    //有多少个英文字母
+    public static String YWZM = "D:/software/sikulix/huishou/ywzm.PNG";
+    public static String YWZM1 = "D:/software/sikulix/huishou/ywzm1.PNG";
+    //犯法吗
+    public static String SRFF = "D:/software/sikulix/huishou/srff.PNG";
+    public static String SRFF1 = "D:/software/sikulix/huishou/srff1.PNG";
+    //吸烟有害吗
+    public static String XY = "D:/software/sikulix/huishou/xy.PNG";
+    public static String XY1 = "D:/software/sikulix/huishou/xy1.PNG";
+    //苹果电话属于国产
+    public static String DH = "D:/software/sikulix/huishou/dh.PNG";
+    public static String DH1 = "D:/software/sikulix/huishou/dh1.PNG";
+    public static List<Object> san2Str(){
+        List<Object> list = new ArrayList<>();
+        list.add(YWZM);
+        list.add(SRFF);
+        list.add(XY);
+        list.add(DH);
+        return list;
+    }
+    //10086
+    public static String ZGYD = "D:/software/sikulix/huishou/zgyd.PNG";
+    public static String ZGYD1 = "D:/software/sikulix/huishou/zgyd1.PNG";
+    //37度是正常体温不
+    public static String TW = "D:/software/sikulix/huishou/tw.PNG";
+    public static String TW1 = "D:/software/sikulix/huishou/tw1.PNG";
+    //天安门在哪个城市
+    public static String TAM = "D:/software/sikulix/huishou/tam.PNG";
+    public static String TAM1 = "D:/software/sikulix/huishou/tam1.PNG";
+    //手表一圈多少小时
+    public static String SB = "D:/software/sikulix/huishou/sb.PNG";
+    public static String SB1 = "D:/software/sikulix/huishou/sb1.PNG";
+    public static List<Object> san3Str(){
+        List<Object> list = new ArrayList<>();
+        list.add(ZGYD);
+        list.add(TW);
+        list.add(TAM);
+        list.add(SB);
+        return list;
     }
 }
--- hlhk_sikulix/src/test/java/com/util/SikHS.java
+++ hlhk_sikulix/src/test/java/com/util/SikHS.java
@@ -1,6 +1,7 @@
 package com.util;
 import org.sikuli.basics.Settings;
 import org.sikuli.script.*;
+import java.util.List;
 
 //回收装备
 public class SikHS {
@@ -50,7 +51,7 @@ public class SikHS {
             region.setY(0);
             region.setW(496);
             region.setH(276);
-            jzbData(region);
+            pdty(region);
             region.setX(0);
             region.setY(0);
             region.setW(1200);
@@ -58,55 +59,48 @@ public class SikHS {
         }
     }
 
-    private static void jzbData(Region region) throws Exception{
-        Match match = null;
-        //矿泉水属于饮料不
+    //判断是哪一组
+    public static void pdty(Region region)throws Exception{
         boolean isTrue = true;
-        match = region.find("D:/software/sikulix/huishou/kqs.PNG");
-        Thread.sleep(500);
-        if(null != match){
-            match = region.find("D:/software/sikulix/huishou/kqs1.PNG");
+        List<Match> list = region.findAnyList(CurrencyData.san1Str());
+        if(!list.isEmpty()){
+          hs1(region);
+          isTrue = false;
+        }
+        if(isTrue){
+            list = region.findAnyList(CurrencyData.san2Str());
+            if(!list.isEmpty()){
+                hs2(region);
+                isTrue = false;
+            }
+        }
+        if(isTrue){
+            list = region.findAnyList(CurrencyData.san3Str());
+            if(!list.isEmpty()){
+                hs3(region);
+                isTrue = false;
+            }
+        }
+        if(isTrue){
+            list = region.findAnyList(CurrencyData.san4Str());
+            if(!list.isEmpty()){
+                hs4(region);
+                isTrue = false;
+            }
+        }
+        //如果没有查到，刷新
+        if(isTrue){
+            //如果一直没有符合的，点击刷新
+            Match match = region.find("D:/software/sikulix/huishou/sxsx.PNG");
             Thread.sleep(500);
             match.click();
-            isTrue = false;
+            pdty(region);
         }
+    }
 
-        //一天多少小时
-        if(isTrue){
-            match = region.find("D:/software/sikulix/huishou/xs.PNG");
-            Thread.sleep(500);
-            if(null != match){
-                match = region.find("D:/software/sikulix/huishou/xs1.PNG");
-                Thread.sleep(500);
-                match.click();
-                isTrue = false;
-            }
-        }
-
-        //赵本山哪国人
-        if(isTrue){
-            match = region.find("D:/software/sikulix/huishou/ngr.PNG");
-            Thread.sleep(500);
-            if(null != match){
-                match = region.find("D:/software/sikulix/huishou/ngr1.PNG");
-                Thread.sleep(500);
-                match.click();
-                isTrue = false;
-            }
-        }
-
-        //爸爸的爸爸叫什么
-        if(isTrue){
-            match = region.find("D:/software/sikulix/huishou/jsm.PNG");
-            Thread.sleep(500);
-            if(null != match){
-                match = region.find("D:/software/sikulix/huishou/jsm1.PNG");
-                Thread.sleep(500);
-                match.click();
-                isTrue = false;
-            }
-        }
-
+    private static void hs4(Region region)throws Exception {
+        boolean isTrue = true;
+        Match match = null;
         //被蚊子叮会不会痒
         if(isTrue){
             match = region.find("D:/software/sikulix/huishou/wz.PNG");
@@ -154,55 +148,23 @@ public class SikHS {
                 isTrue = false;
             }
         }
-
-        //有多少个英文字母
         if(isTrue){
-            match = region.find("D:/software/sikulix/huishou/ywzm.PNG");
+            //如果一直没有符合的，点击刷新
+            match = region.find("D:/software/sikulix/huishou/sxsx.PNG");
             Thread.sleep(500);
-            if(null != match){
-                match = region.find("D:/software/sikulix/huishou/ywzm1.PNG");
-                Thread.sleep(500);
-                match.click();
-                isTrue = false;
-            }
+            match.click();
+            pdty(region);
         }
-
-        //犯法吗
-        if(isTrue){
-            match = region.find("D:/software/sikulix/huishou/srff.PNG");
-            Thread.sleep(500);
-            if(null != match){
-                match = region.find("D:/software/sikulix/huishou/srff1.PNG");
-                Thread.sleep(500);
-                match.click();
-                isTrue = false;
-            }
+        match = region.wait("D:/software/sikulix/image/hs5.PNG",1);
+        if(null == match){
+            match = region.wait("D:/software/sikulix/image/hs7.PNG",1);
         }
+        match.click();
+    }
 
-        //吸烟有害吗
-        if(isTrue){
-            match = region.find("D:/software/sikulix/huishou/xy.PNG");
-            Thread.sleep(500);
-            if(null != match){
-                match = region.find("D:/software/sikulix/huishou/xy1.PNG");
-                Thread.sleep(500);
-                match.click();
-                isTrue = false;
-            }
-        }
-
-        //苹果电话属于国产
-        if(isTrue){
-            match = region.find("D:/software/sikulix/huishou/dh.PNG");
-            Thread.sleep(500);
-            if(null != match){
-                match = region.find("D:/software/sikulix/huishou/dh1.PNG");
-                Thread.sleep(500);
-                match.click();
-                isTrue = false;
-            }
-        }
-
+    private static void hs3(Region region)throws Exception {
+        boolean isTrue = true;
+        Match match = null;
         //10086
         if(isTrue){
             match = region.find("D:/software/sikulix/huishou/zgyd.PNG");
@@ -250,13 +212,12 @@ public class SikHS {
                 isTrue = false;
             }
         }
-
         if(isTrue){
             //如果一直没有符合的，点击刷新
             match = region.find("D:/software/sikulix/huishou/sxsx.PNG");
             Thread.sleep(500);
             match.click();
-            jzbData(region);
+            pdty(region);
         }
         match = region.wait("D:/software/sikulix/image/hs5.PNG",1);
         if(null == match){
@@ -264,4 +225,132 @@ public class SikHS {
         }
         match.click();
     }
+
+    private static void hs2(Region region)throws Exception {
+        boolean isTrue = true;
+        Match match = null;
+        //有多少个英文字母
+        if(isTrue){
+            match = region.find("D:/software/sikulix/huishou/ywzm.PNG");
+            Thread.sleep(500);
+            if(null != match){
+                match = region.find("D:/software/sikulix/huishou/ywzm1.PNG");
+                Thread.sleep(500);
+                match.click();
+                isTrue = false;
+            }
+        }
+
+        //犯法吗
+        if(isTrue){
+            match = region.find("D:/software/sikulix/huishou/srff.PNG");
+            Thread.sleep(500);
+            if(null != match){
+                match = region.find("D:/software/sikulix/huishou/srff1.PNG");
+                Thread.sleep(500);
+                match.click();
+                isTrue = false;
+            }
+        }
+
+        //吸烟有害吗
+        if(isTrue){
+            match = region.find("D:/software/sikulix/huishou/xy.PNG");
+            Thread.sleep(500);
+            if(null != match){
+                match = region.find("D:/software/sikulix/huishou/xy1.PNG");
+                Thread.sleep(500);
+                match.click();
+                isTrue = false;
+            }
+        }
+
+        //苹果电话属于国产
+        if(isTrue){
+            match = region.find("D:/software/sikulix/huishou/dh.PNG");
+            Thread.sleep(500);
+            if(null != match){
+                match = region.find("D:/software/sikulix/huishou/dh1.PNG");
+                Thread.sleep(500);
+                match.click();
+                isTrue = false;
+            }
+        }
+        if(isTrue){
+            //如果一直没有符合的，点击刷新
+            match = region.find("D:/software/sikulix/huishou/sxsx.PNG");
+            Thread.sleep(500);
+            match.click();
+            pdty(region);
+        }
+        match = region.wait("D:/software/sikulix/image/hs5.PNG",1);
+        if(null == match){
+            match = region.wait("D:/software/sikulix/image/hs7.PNG",1);
+        }
+        match.click();
+    }
+
+    private static void hs1(Region region) throws Exception{
+        boolean isTrue = true;
+        Match match = null;
+        //矿泉水属于饮料不
+        match = region.find("D:/software/sikulix/huishou/kqs.PNG");
+        Thread.sleep(500);
+        if(null != match){
+            match = region.find("D:/software/sikulix/huishou/kqs1.PNG");
+            Thread.sleep(500);
+            match.click();
+            isTrue = false;
+        }
+
+        //一天多少小时
+        if(isTrue){
+            match = region.find("D:/software/sikulix/huishou/xs.PNG");
+            Thread.sleep(500);
+            if(null != match){
+                match = region.find("D:/software/sikulix/huishou/xs1.PNG");
+                Thread.sleep(500);
+                match.click();
+                isTrue = false;
+            }
+        }
+
+        //赵本山哪国人
+        if(isTrue){
+            match = region.find("D:/software/sikulix/huishou/ngr.PNG");
+            Thread.sleep(500);
+            if(null != match){
+                match = region.find("D:/software/sikulix/huishou/ngr1.PNG");
+                Thread.sleep(500);
+                match.click();
+                isTrue = false;
+            }
+        }
+
+        //爸爸的爸爸叫什么
+        if(isTrue){
+            match = region.find("D:/software/sikulix/huishou/jsm.PNG");
+            Thread.sleep(500);
+            if(null != match){
+                match = region.find("D:/software/sikulix/huishou/jsm1.PNG");
+                Thread.sleep(500);
+                match.click();
+                isTrue = false;
+            }
+        }
+
+        if(isTrue){
+            //如果一直没有符合的，点击刷新
+            match = region.find("D:/software/sikulix/huishou/sxsx.PNG");
+            Thread.sleep(500);
+            match.click();
+            pdty(region);
+        }
+        match = region.wait("D:/software/sikulix/image/hs5.PNG",1);
+        if(null == match){
+            match = region.wait("D:/software/sikulix/image/hs7.PNG",1);
+        }
+        match.click();
+    }
+
 }
