bc9f69298243a936cc801937cda467bf7f388044
466972777@qq.com <466972777@qq.com>
2023-03-05 07:38:08 +0800

    asdf

--- hlhk_sikulix/src/test/java/com/util/BaoBao.java
+++ hlhk_sikulix/src/test/java/com/util/BaoBao.java
@@ -13,9 +13,9 @@ public class BaoBao {
 
     public static void baobao(Region region)throws Exception{
         SikJZB.gb(region);
-        Match match = region.find("D:/software/sikulix/image/baobao3.PNG");
+        Match match = region.find("D:/software/sikulix/image/baobao2.PNG");
         if(null == match){
-             match = region.find("D:/software/sikulix/image/baobao3.PNG");
+             match = region.find("D:/software/sikulix/image/baobao2.PNG");
              if(null == match){
                  match = region.find("D:/software/sikulix/image/bbzh.PNG");
                  if(null == match){
