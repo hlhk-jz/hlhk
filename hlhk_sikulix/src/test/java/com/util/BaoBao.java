36ef6a83a3e8c4536cd9c06d7cd60f76cbba79d6
466972777@qq.com <466972777@qq.com>
2023-03-12 13:42:56 +0800

    asdf

--- hlhk_sikulix/src/test/java/com/util/BaoBao.java
+++ hlhk_sikulix/src/test/java/com/util/BaoBao.java
@@ -5,26 +5,32 @@ import org.sikuli.script.Region;
 
 //检查宝宝是否挂了
 public class BaoBao {
-   /* public static void main(String[] args)throws Exception{
+    public static void main(String[] args)throws Exception{
         Region region = new Region(0, 0,1200,800);
         region.setThrowException(false);
         baobao(region);
-    }*/
+    }
 
     public static void baobao(Region region)throws Exception{
         SikJZB.gb(region);
         Match match = region.find("D:/software/sikulix/image/baobao6.PNG");
         while (null == match){
-             match = region.find("D:/software/sikulix/image/baobao6.PNG");
+            SikJZB.gb(region);
+            match = region.find("D:/software/sikulix/image/baobao6.PNG");
+            Thread.sleep(500);
              if(null == match){
+                 System.out.println("检测到宝宝不在线，召唤中！！！！！！");
                  match = region.find("D:/software/sikulix/image/bbzh.PNG");
+                 Thread.sleep(500);
                  if(null == match){
                      match = region.find("D:/software/sikulix/image/bbzh.PNG");
                  }
+                 Thread.sleep(500);
                  match.click();
                  match.setX(match.getX()-30);
                  match.hover();
                  match = region.wait("D:/software/sikulix/image/baobao6.PNG",5);
+                 Thread.sleep(500);
              }
         }
     }
