012cdb78b6b9b12305fb45d30e1e25e778bc3082
466972777@qq.com <466972777@qq.com>
2023-03-29 19:38:18 +0800

    asdf

--- hlhk_sikulix/src/test/java/com/util/SikHS.java
+++ hlhk_sikulix/src/test/java/com/util/SikHS.java
@@ -12,7 +12,7 @@ import java.util.Set;
 
 //回收装备
 public class SikHS {
-    /* public static void main(String[] args){
+     public static void main(String[] args){
         try {
             Region region = new Region(0, 0,1200,800);
             region.setThrowException(false);
@@ -20,7 +20,7 @@ public class SikHS {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }*/
+    }
 
     public static void hs(Region region)throws Exception{
         Settings.MinSimilarity = 0.7;
