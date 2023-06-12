059960a62a61255065b21887b84eb80e674a17a4
466972777@qq.com <466972777@qq.com>
2023-06-13 07:18:51 +0800

    asdf

--- hlhk_sikulix/src/test/java/wd/KS.java
+++ hlhk_sikulix/src/test/java/wd/KS.java
@@ -1,20 +1,16 @@
 package wd;
-import org.sikuli.basics.Settings;
 import org.sikuli.script.Match;
 import org.sikuli.script.Region;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 public class KS {
     public static void main(String[] args){
         try {
             Region region1 = new Region(1678, 26,242,255);
             Match region = region1.wait(KSData.sikpng, 2);
-            SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
             while (true){
                 try {
                     Match wait = KSData.regionData.wait(KSData.DATA, 10);
                     if(null != wait){
-                        Match match = KSData.region1.wait(KSData.DATA, 0.5);
+                        Match match = KSData.region1.wait(KSData.DATA, 0.3);
                         if(null != match){
                             match.click();
                             Thread.sleep(300);
@@ -24,7 +20,7 @@ public class KS {
                             match.click();
                         }
 
-                        match = KSData.region2.wait(KSData.DATA, 0.5);
+                        match = KSData.region2.wait(KSData.DATA, 0.3);
                         if(null != match){
                             match.click();
                             Thread.sleep(300);
@@ -34,7 +30,7 @@ public class KS {
                             match.click();
                         }
 
-                        match = KSData.region3.wait(KSData.DATA, 0.5);
+                        match = KSData.region3.wait(KSData.DATA, 0.3);
                         if(null != match){
                             match.click();
                             Thread.sleep(300);
@@ -44,7 +40,7 @@ public class KS {
                             match.click();
                         }
 
-                        match = KSData.region4.wait(KSData.DATA, 0.5);
+                        match = KSData.region4.wait(KSData.DATA, 0.3);
                         if(null != match){
                             match.click();
                             Thread.sleep(300);
@@ -54,7 +50,7 @@ public class KS {
                             match.click();
                         }
 
-                        match = KSData.region5.wait(KSData.DATA, 0.5);
+                        match = KSData.region5.wait(KSData.DATA, 0.3);
                         if(null != match){
                             match.click();
                             Thread.sleep(300);
@@ -63,7 +59,6 @@ public class KS {
                             match.setY(match.getY()+25);
                             match.click();
                         }
-                        System.out.println("执行时间："+format.format(new Date()));
                         region.hover();
                     }
                 }catch (Exception e ){
