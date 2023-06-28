8ec58a848d39b1167bad492dfcc8b7e9079f1bec
jiangzhi <zhi.jiang@zkang-e.com>
2023-06-28 09:37:31 +0800

    调节xy

--- hlhk_https/src/main/java/com/controller/TestController.java
+++ hlhk_https/src/main/java/com/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
@@ -9,5 +8,15 @@ public class TestController {
     @GetMapping("/get/test")
     public Object show(){
         return "成功";
+    }
+
+    @PostMapping("/post/test")
+    public Object test(@RequestBody Object obg){
+        return obg;
+    }
+
+    @GetMapping("/get/test/{value}")
+    public Object show1(@PathVariable("value") String value){
+        return value;
     }
 }
