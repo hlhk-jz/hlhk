dce8d1116f1a78c8d3459b15af68fc96c653d669
jiangzhi <zhi.jiang@zkang-e.com>
2023-08-16 11:36:34 +0800

    调节xy

---
+++ hlhk_test/src/main/java/com/controller/WxTest.java
@@ -0,0 +1,106 @@
+package com.controller;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.lang.WeightRandom;
+import cn.hutool.core.util.RandomUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+public class WxTest {
+    public static List<Integer> list = new ArrayList<>();
+    static {
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+
+        list.add(3);
+        list.add(3);
+        list.add(3);
+        list.add(3);
+        list.add(3);
+        list.add(3);
+        list.add(3);
+        list.add(3);
+        list.add(3);
+        list.add(3);
+        list.add(3);
+        list.add(3);
+
+        list.add(4);
+        list.add(4);
+        list.add(4);
+        list.add(4);
+        list.add(4);
+
+        list.add(5);
+    }
+
+    public static int num = 0;
+    @GetMapping("/wuxing/test")
+    public Integer get(){
+        int c = RandomUtil.randomEle(list);
+        num ++;
+        return c;
+    }
+}
