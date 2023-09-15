d7fa65c2626be06aa00cd6663c453da7fe820726
jiangzhi <zhi.jiang@zkang-e.com>
2023-09-15 12:06:57 +0800

    调节xy

--- html-webcollector/src/test/java/com/DemoAutoNewsCrawler.java
+++ html-webcollector/src/test/java/com/DemoAutoNewsCrawler.java
@@ -2,35 +2,25 @@ package com;
 import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
 import cn.edu.hfut.dmic.webcollector.model.Page;
 import cn.edu.hfut.dmic.webcollector.plugin.rocks.BreadthCrawler;
-
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 public class DemoAutoNewsCrawler  extends BreadthCrawler{
+    public static Map<String,String> urlMap = new HashMap<>();
+    public static Map<String,String> map = new LinkedHashMap<>();
+
     public DemoAutoNewsCrawler(String crawlPath, boolean autoParse) {
         super(crawlPath, autoParse);
     }
-    public List<String > list = new ArrayList<>();
-
     @Override
     public void visit(Page page, CrawlDatums next) {
-        File file = new File("D:/saoyang.txt");
-        BufferedWriter writer = null;
-        try {
-            writer = new BufferedWriter(new FileWriter(file));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         //获取地址
-        String url = page.url();
+        String url = page.url().replace("#", "");
         if(null != url && !"".equals(url)){
             //获取抓取页面的内容
             String body = page.select("body").first().text();
-            if(body.contains("并刷新页面") && body.contains("『加入书签，方便阅读』")){
+            if(body.contains("并刷新页面") && body.contains("『加入书签，方便阅读』") && !body.contains("番外")
+                    && body.contains("第") && body.contains("字体")  ){
                 String str =body;
                 String str8=str.substring(0, str.indexOf("第"));
                 String strtitle=str.substring(str8.length(), str.length());
@@ -40,8 +30,22 @@ public class DemoAutoNewsCrawler  extends BreadthCrawler{
                 String str3 = str2.substring(0,str2.indexOf("『加入书签，方便阅读』") );
                 String str5 = str3.replace("　", "");
                 String str6 = str5.replace("  ", "\r");
-                test.list.add(title);
-                test.list.add(str6);
+
+                String mUrl = DemoAutoNewsCrawler.urlMap.get(title);
+                //如果是新的存入
+                if(null == mUrl || "".equals(mUrl)){
+                    DemoAutoNewsCrawler.urlMap.put(title,url);
+                    DemoAutoNewsCrawler.map.put(title, str6);
+                }else {
+                    //如果是旧的并且是第二页
+                    if(!url.equals(mUrl)){
+                      /*  System.out.println(url);
+                        System.out.println(mUrl);
+                        System.out.println(DemoAutoNewsCrawler.map.get(title));
+                        System.out.println(str6);*/
+                        DemoAutoNewsCrawler.map.put(title, DemoAutoNewsCrawler.map.get(title)+str6);
+                    }
+                }
             }
         }
     }
@@ -64,13 +68,15 @@ public class DemoAutoNewsCrawler  extends BreadthCrawler{
         //同时启动多少个线程进行抓取
         crawler.setThreads(1);
         //启动爬虫,向下爬10层
-        crawler.start(2);
+        crawler.start(3);
         // 创建一个文件输出流
         try {
             FileWriter writer = new FileWriter("D:/saoyang.txt");
             // 将List的内容写入文件
-            for (String item : test.list) {
-                writer.write(item + "\n");
+            Set<String> strings = DemoAutoNewsCrawler.map.keySet();
+            for (String str : strings){
+                writer.write(str + "\n");
+                writer.write( DemoAutoNewsCrawler.map.get(str) + "\n");
             }
             writer.close();
         } catch (IOException e) {
