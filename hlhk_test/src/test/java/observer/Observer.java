1f8cf4595daa29b57115c6adfc412a4627d7b58e
jiangzhi <zhi.jiang@zkang-e.com>
2024-01-18 10:19:09 +0800

    jsql

---
+++ hlhk_test/src/test/java/observer/Observer.java
@@ -0,0 +1,8 @@
+package observer;
+
+/**
+ * 抽象观察者
+ */
+public interface Observer {
+    public void update(String message);
+}
---
+++ hlhk_test/src/test/java/observer/Observerable.java
@@ -0,0 +1,10 @@
+package observer;
+
+/**
+ * 抽象被观察者接口
+ */
+public interface Observerable {
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObserver();
+}
