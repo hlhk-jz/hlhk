41dd07d2c62b7a448ceed4110d3c63c7f620de3d
jiangzhi <zhi.jiang@zkang-e.com>
2024-05-14 09:13:21 +0800

    jsql

--- hlhk_sikulix/src/test/java/jincheng/Win32.java
+++ hlhk_sikulix/src/test/java/jincheng/Win32.java
@@ -1,15 +1,32 @@
 package jincheng;
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.*;
 
-import com.sun.jna.platform.win32.User32;
-import com.sun.jna.platform.win32.WinDef;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 //https://learn.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-adjustwindowrect
 public class Win32 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        //获取计算器的窗口句柄
-        WinDef.HWND hwnd = User32.INSTANCE.FindWindow(null,"设置");
-        //将窗口带到前台
-        User32.INSTANCE.SetForegroundWindow(hwnd);
+        User32 user32 = User32.INSTANCE;
+        WinDef.HWND hwnd = user32.FindWindow(null, "test");
+
+
+
+
+        WinUser.HOOKPROC hookproc = null;
+        WinUser.HHOOK hhook = user32.SetWindowsHookEx(2, hookproc,  Kernel32.INSTANCE.GetModuleHandle(null),21772 );
+        if(null != hwnd){
+            //激活并显示窗口。并且系统会将其还原到其原始大小和位置。
+            //user32.ShowWindow(hwnd, 1);
+            //将创建指定窗口的线程引入前台
+            //boolean b = user32.SetForegroundWindow(hwnd);
+        }else {
+            System.out.println("未查询到指定信息");
+        }
+
+
 
     }
 }
